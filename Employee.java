e180708af49114846665c7e99668a74f76b797c1
shankar-trainer <shankar7979@hotmail.com>
2023-09-21 10:43:38 +0530

    added

---
+++ Employee.java
@@ -0,0 +1,23 @@
+public class Employee {
+
+    private int id;
+    private String name;
+    private float salary;
+
+  public Employee(int id, String name, float salary) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+    }
+}
+
+class  Manager extends Employee{
+
+
+}
+class ManagerMain {
+    public static void main(String[] args) {
+ //       Manager manager=new Manager();
+
+    }
+}
